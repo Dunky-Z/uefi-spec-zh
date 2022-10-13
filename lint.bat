fe19cb35881adc70fb3c9dc933a58dcbf49d8dec
Dunky-Z <254758318@qq.com>
2022-10-13 10:53:15 +0800

    doc:update introduction

--- lint.bat
+++ lint.bat
@@ -1,5 +1,13 @@
 cd ./src
-dir
-autocorrect.exe --fix 1-Introduction.md 2-Overview.md
 
+@REM Just for test
+dir 
+
+@REM Use autocorrect to fix typo 
+autocorrect.exe --fix ./*.md
+
+@REM Use markdownlint to check markdown style
+markdownlint -c ../.markdownlint.json -f 1-Introduction.md 2-Overview.md 
+
+@REM Use pandoc to build pdf
 pandoc.exe -f  markdown-auto_identifiers --pdf-engine=xelatex   --template=mppl.latex -s --listings  1-Introduction.md 2-Overview.md  -o ../build/UEFI-Spec-zh.pdf
